c3dabe4b921674fa8daf4332d1f0193851bcb211
Louis-star256 <louisdavinic256@gmail.com>
2026-05-15 21:47:28 +0300

    Add files via upload

---
+++ firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  "projectId": "studio-1216835987-c72bd",
+  "appId": "1:458819824281:web:ede9d786eecc860e23abc3",
+  "apiKey": "AIzaSyDIzE-fwTT-5kZ31qcTl34UmwQLhZj2Fwk",
+  "authDomain": "studio-1216835987-c72bd.firebaseapp.com",
+  "storageBucket": "studio-1216835987-c72bd.firebasestorage.app",
+  "measurementId": "",
+  "messagingSenderId": "458819824281"
+};
